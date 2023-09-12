56cd6559d30d67315641744af19b6517886e6fea
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2023-09-12 06:52:36 -0700

    LapU and discLapU gets privacy_level as input

---
+++ LapU.py
@@ -0,0 +1,15 @@
+class LapU:
+    def __init__(self, cuda_device):
+        self.cuda_device = cuda_device
+
+
+def privatize(self, tst_data_y, tst_data_z, alpha):
+        d = self.kappa ** tst_data_y.size(dim = 1)
+        theta = d**(1/2)
+        tst_data_y_multi = self.h_bin(tst_data_y, self.kappa)
+        tst_data_y_oneHot = self.transform_onehot(tst_data_y_multi, d)
+        tst_data_z_multi = self.h_bin(tst_data_z, self.kappa) 
+        tst_data_z_oneHot = self.transform_onehot(tst_data_z_multi, d)
+        dataCombined = torch.cat([tst_data_y_oneHot, tst_data_z_oneHot], dim = 0)
+        tst_data_priv = self.LapU(dataCombined, alpha, 2, theta)
+        return(tst_data_priv)
--- client.py
+++ client.py
@@ -2,21 +2,21 @@ class client:
     def __init__(self, privacy_level, cuda_device):
         self.privacy_level = privacy_level
         self.cuda_device = cuda_device
-        self.discretizer = discretizer()
-        self.LapU = LapU()
-        self.discLapU = discLapU()
+        self.discretizer = discretizer(cuda_device)
+        self.LapU = LapU(cuda_device)
+        self.discLapU = discLapU(cuda_device)
 
     def load_data_disc(self, data):
         self.data = data
 
     def load_data_conti(self, data, n_bin):
-        self.data = discretizer.transform(data, n_bin)
+        self.data = self.discretizer.transform(data, n_bin)
     
     def release_LapU(self):
-        return(self.LapU.privatize(self.data))
+        return(self.LapU.privatize(self.data), self.privacy_level)
     
-    def release_DiscLapU(self)
-        return(self.discLapU.privatize(self.data))
+    def release_DiscLapU(self):
+        return(self.discLapU.privatize(self.data), self.privacy_level)
 
    
 
