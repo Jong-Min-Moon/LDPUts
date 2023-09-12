17e6f89346c325b6ea6d9b654eb8805b5535c029
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2023-09-12 11:36:46 -0700

    separeted noise generation in LapU from privatize

--- privacy_mechanism.py
+++ privacy_mechanism.py
@@ -7,17 +7,16 @@ class LapU:
 
 
     def privatize(self, data_mutinomial, alphabet_size, privacy_level):
-        data_onehot = self.__transform_onehot(data_mutinomial, alphabet_size)
-        laplace_scale = torch.tensor(2*alphabet_size).sqrt().mul(2).div(privacy_level).to(self.cuda_device) #sigma_alpha in the paper
         sample_size = self.__get_sample_size(data_mutinomial)
+        data_onehot_scaled = self.__transform_onehot(data_mutinomial, alphabet_size).mul(alphabet_size**(1/2)) # scaled by \sqrt(k)
+        noise = self.__generate_noise(alphabet_size, privacy_level, sample_size)
+        return(torch.add(data_onehot_scaled,noise))
+           
+    def __generate_noise(self, alphabet_size, privacy_level, sample_size):
+        laplace_scale = torch.tensor(2*alphabet_size).sqrt().mul(2).div(privacy_level).to(self.cuda_device) #sigma_alpha in the paper
         laplace_noise = self.unit_laplace_generator.sample(sample_shape = torch.Size([sample_size, alphabet_size])).to(self.cuda_device)
-        return(
-            torch.add(
-                data_onehot.mul(alphabet_size**(1/2)),
-                laplace_noise.mul(laplace_scale)
-            )  
-        )       
-    
+        return(laplace_noise.mul(laplace_scale))
+
     def __transform_onehot(self, data_multinomial, alphabet_size):
         return(torch.nn.functional.one_hot(data_multinomial, alphabet_size))
 
@@ -34,3 +33,4 @@ class LapU:
             torch.tensor(0.0).to(self.cuda_device),
             torch.tensor(2**(-1/2)).to(self.cuda_device)
         )
+
