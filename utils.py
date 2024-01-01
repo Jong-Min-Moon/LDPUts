eea542bdb10222523e7f6da9c124bc6042663ebf
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2023-12-31 16:18:06 -0800

    sync with jupyter server

--- utils.py
+++ utils.py
@@ -1,4 +1,12 @@
 import torch
+
+def projection_orth_one(alphabet_size):
+    identity = torch.eye(alphabet_size)
+    one_one_t = torch.ones( torch.Size([alphabet_size,alphabet_size]) )
+    one_one_t_over_d = one_one_t.div(alphabet_size)
+    one_projector = identity.sub(one_one_t_over_d)
+    return(one_projector)
+
 def get_sample_size(data):
     if data.dim() == 1:
         return( data.size(dim = 0) )
@@ -15,3 +23,6 @@ def get_dimension(data):
         return( data.size(dim = 1) )
     else:
         return # we only use up to 2-dimensional tensor, i.e. matrix
+    
+def chi_sq_dist(x1,x2):
+    return( torch.norm((x1-x2)/((x1+x2).sqrt()), p=2))
